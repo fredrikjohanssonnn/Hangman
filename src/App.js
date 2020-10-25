a8f0d27d426f103db8d5aea14a8286f7f6807b10
fredrikjohanssonnn <fjohansson.creative@gmail.com>
2020-10-25 11:25:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment, useState } from 'react';
 import './App.css';
 
-function App() {
+import Header from './components/Header';
+import Figure from './components/Figure';
+import WrongLetters from './components/WrongLetters';
+import Word from './components/Word';
+import Popup from './components/Popup';
+import Notification from './components/Notification';
+
+const words = ['application', 'programming', 'interface', 'wizard'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+const App = () => {
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header />
+      <div className='game-container'>
+        <Figure />
+        <WrongLetters wrongLetters={wrongLetters} />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
+      </div>
+    </Fragment>
   );
-}
+};
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+const Header = () => {
+  return (
+    <Fragment>
+      <h1>Hangman</h1>
+      <p>Find the hidden word</p>
+    </Fragment>
+  );
+};
+
+export default Header;
